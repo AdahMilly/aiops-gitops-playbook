c96c29c9e3f2c6e7e6d277c7f4f0463d8ea01707
AdahMilly <oamildred@gmail.com>
2026-07-15 15:58:41 +0300

    feat:monitoring

---
+++ otel.ts
@@ -0,0 +1,22 @@
+import { NodeSDK } from "@opentelemetry/sdk-node";
+import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-grpc";
+import { resourceFromAttributes } from "@opentelemetry/resources";
+import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
+
+const sdk = new NodeSDK({
+  resource: resourceFromAttributes({
+    [SemanticResourceAttributes.SERVICE_NAME]:
+      "aiops-gitops-playbook",
+  }),
+
+  traceExporter: new OTLPTraceExporter(),
+
+  instrumentations: [
+    getNodeAutoInstrumentations(),
+  ],
+});
+
+sdk.start();
+
+console.log("OpenTelemetry initialized");
